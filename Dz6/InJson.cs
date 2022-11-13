dd7975d9f455e0154126991e54aba220d32c892d
endespair <endespair@DESKTOP-CHK0J9Q>
2022-11-13 13:55:04 +0200

    Добавьте файлы проекта.

---
+++ Dz6/InJson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz6
+{
+    internal class InJson
+    {
+        //[JsonIgnore]
+        public int PublishingHouseId { get; set; }
+        //[JsonPropertyName("Name")]
+        public string? Title { get; set; }
+        public PublishingHouse? PublishingHouse { get; set; }
+    }
+}
---
+++ Dz6/Program.cs
@@ -0,0 +1,11 @@
+﻿using Dz6;
+using System.Text.Json;
+
+
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+const string path = "./JSON_info.json";
+
+var jsonContent = File.ReadAllText(path);
+
+var info = JsonSerializer.Deserialize<List<InJson>>(jsonContent);
